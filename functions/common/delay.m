392f450c8561d37c0e46f143156aecbfdc850c4e
Deyan Dobromirov <dvd_video@abv.bg>
2018-04-17 17:00:50 +0300

    Update delay.m

--- functions/common/delay.m
+++ functions/common/delay.m
@@ -1,5 +1,13 @@
-function [] = delay(len)
+function out = delay(len)
+  % Cycles a number of times as the value in the len parameter
+  % Example:
+  % delay(100)
+  % 
+  % ans =
+  % 
+  % -1
   while(len)
     len = len - 1;
   end
+  out = len
 end
